fce68f96281b35d7335790e22f4a9a18601a90cc
Nick Doran <nicksums23@gmail.com>
2016-05-19 14:43:31 -0400

    +package structure, +maven shade

---
+++ src/main/java/org/dungeonrealms/DungeonRealms.java
@@ -0,0 +1,18 @@
+package org.dungeonrealms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Dr. Nick Doran on 5/19/2016.
+ */
+public class DungeonRealms extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
