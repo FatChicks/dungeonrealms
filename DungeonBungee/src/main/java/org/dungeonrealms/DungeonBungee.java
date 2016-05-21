e3c6596030c14e30d6773aeb9ba4d1d4a717aa88
Nick Doran <nicksums23@gmail.com>
2016-05-21 15:11:08 -0400

    +Bungee Module

---
+++ DungeonBungee/src/main/java/org/dungeonrealms/DungeonBungee.java
@@ -0,0 +1,18 @@
+package org.dungeonrealms;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * Created by Dr. Nick Doran on 5/21/2016.
+ */
+public class DungeonBungee extends Plugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
